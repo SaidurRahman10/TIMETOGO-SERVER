22766dfc5ddafe09de27f4e44a4593147745fc02
Saidur Rahman <saidurrahman1473@gmail.com>
2022-11-10 20:05:43 +0600

    fix update side error

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion , ObjectId} = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 //middle wares
 
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qxzlll3.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -23,7 +22,6 @@ const run = async () => {
     const placeCollection = client.db("timetogo").collection("place");
     const orderCollection = client.db("timetogo").collection("order");
 
-
     app.get("/place", async (req, res) => {
       const query = {};
       const data = placeCollection.find(query);
@@ -32,112 +30,90 @@ const run = async () => {
     });
     app.get("/place/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: ObjectId(id)};
-      const place = await placeCollection.findOne(query)
-      res.send(place)
+      const query = { _id: ObjectId(id) };
+      const place = await placeCollection.findOne(query);
+      res.send(place);
     });
 
     app.get("/allPlace", async (req, res) => {
-        const query = {};
-        const data = placeCollection.find(query);
-        const cursor = await data.toArray();
-        res.send(cursor);
+      const query = {};
+      const data = placeCollection.find(query);
+      const cursor = await data.toArray();
+      res.send(cursor);
     });
-
 
     //add korbo Places
-    
-    app.post("/allPlace", async (req, res) => {
 
+    app.post("/allPlace", async (req, res) => {
       const place = req.body;
-      const result =  await placeCollection.insertOne(place)
-      res.send(result)
-    
-  });
-    
-    
-    app.get("/allPlace/:id", async(req, res) => {
-        const id = req.params.id;
-        const query = {_id: ObjectId(id)};
-        const place = await placeCollection.findOne(query)
-        res.send(place)
+      const result = await placeCollection.insertOne(place);
+      res.send(result);
     });
 
+    app.get("/allPlace/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const place = await placeCollection.findOne(query);
+      res.send(place);
+    });
 
     //orders api
-    app.get('/orders',async(req,res)=>{
-        let query = {};
-        if(req.query.email){
-            query={
-                email: req.query.email
-            }
-        }
-        const cursor = orderCollection.find(query)
-        const  orders = await cursor.toArray()
-        res.send(orders)
+    app.get("/orders", async (req, res) => {
+      let query = {};
+      if (req.query.email) {
+        query = {
+          email: req.query.email,
+        };
+      }
+      const cursor = orderCollection.find(query);
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
 
-    })
-
-   app.get('/orders/:id',async(req,res)=>{
-
-    const id = req.params.id;
-    const query = {_id: ObjectId(id)};
-    const place = await orderCollection.findOne(query);
-    res.send(place)
-    
-   })
-
-    app.post('/orders',async(req,res)=>{
-        const order = req.body;
-        const result = await orderCollection.insertOne(order);
-        res.send(result)
-    })
-   
-    //Update
-    app.put('/orders/:id', async(req, res)=>{
+    app.get("/orders/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: ObjectId(id)};
+      const query = { _id: ObjectId(id) };
+      const place = await orderCollection.findOne(query);
+      res.send(place);
+    });
+
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    });
+
+    //Update
+    app.put("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
       const user = req.body;
-      const option = {upsert: true};
+      const option = { upsert: true };
       const updatedUser = {
         $set: {
-          message: user.message
-        }
-      }
-   
+          message: user.message,
+        },
+      };
 
+      const result = await orderCollection.updateOne(
+        filter,
+        updatedUser,
+        option
+      );
+      res.send(result);
+    });
 
-
-
-    // delete 
-    app.delete('/orders/:id', async(req,res)=>{
-
-        const id = req.params.id;
-        const query = {_id: ObjectId(id)};
-        const result = await orderCollection.deleteOne(query)
-        res.send(result)
-    })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    // delete
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 };
 run().catch((error) => console.log(error));
-
-
 
 app.get("/", (req, res) => {
   res.send("Tourist Server is Running");
